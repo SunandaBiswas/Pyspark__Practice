2192c470e47abc393e0904ca0c793d9539f50b59
Sunanda Biswas <81425092+SunandaBiswas@users.noreply.github.com>
2022-08-10 17:15:14 +0600

    Update Loading_data.py

--- Loading_data.py
+++ Loading_data.py
@@ -8,3 +8,15 @@ partners = spark.table('data_table.partners')
 
 display(transactions)
 display(partners)
+
+
+# checking all the column names and data types
+
+transactions.printSchema()
+partners.printSchema()
+
+
+# creating transactions dataframe using relevant features
+
+transactions_df = transactions.select("id","account_id","partner_id", "transaction_type","channel", "transaction_date","total_value","incentive_value","partner_code","currency_code")
+transactions_df.limit(5).toPandas()
